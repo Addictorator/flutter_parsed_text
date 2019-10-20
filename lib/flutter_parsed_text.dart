92a642f9c01890531a84af23759122065e76bac9
Guy <ghaisanlatheef98@gmail.com>
2019-10-20 11:59:24 +0500

    AI-3.5.1 <Guy@Guy-PC Overwrite remote https://github.com/Addictorator/flutter_parsed_text to local

--- lib/flutter_parsed_text.dart
+++
@@ -1,9 +0,0 @@
-library flutter_parsed_text;
-
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
-
-part 'src/parsed_text.dart';
-part 'src/regex_options.dart';
-part 'src/match_text.dart';
-part 'src/constants.dart';
